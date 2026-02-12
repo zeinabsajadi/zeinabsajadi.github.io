e7258907c977635f0464cca3ac01f7db18a100ca
zeinabsajadi <zeinabsajadynasab@gmail.com>
2026-02-12 12:15:40 +0330

    add model

--- Backend/blog/models.py
+++ Backend/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Tech(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.ImageField(upload_to='tech_icons/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    skills = models.ForeignKey(Tech, on_delete=models.CASCADE, related_name='projects')
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.URLField(max_length=200)
+    icon = models.ImageField(upload_to='contact_icons/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
