36bdb53b8363321145bf450a17ec2eaa6caeea04
zeinabsajadi <zeinabsajadynasab@gmail.com>
2026-02-12 12:32:04 +0330

    add views

--- Backend/blog/views.py
+++ Backend/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Tech, Contact, Course, Projects
+from .serializers import TechSerializer, ProjectsSerializer, ContactSerializer, CourseSerializer
 
-# Create your views here.
+
+class TechViewSet(viewsets.ModelViewSet):
+    queryset = Tech.objects.all()
+    serializer_class = TechSerializer
+
+
+class ProjectsViewSet(viewsets.ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
