4e7a05d2a5bd04ec2a4249fd35eca2656b6aff90
zeinabsajadi <zeinabsajadynasab@gmail.com>
2026-02-22 10:44:51 +0330

    debug router

--- Backend/blog/urls.py
+++ Backend/blog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TechViewSet, CourseViewSet, ContactViewSet, ProjectsViewSet 
+from . import views
 
 router = DefaultRouter()
-router.register(r'tech', TechViewSet)
-router.register(r'projects', ProjectsViewSet)
-router.register(r'courses', CourseViewSet)
-router.register(r'contacts', ContactViewSet)
+router.register(r'techs', views.TechViewSet, basename='tech')
+router.register(r'projects', views.ProjectsViewSet, basename='project')
+router.register(r'courses', views.CourseViewSet, basename='course')
+router.register(r'contacts', views.ContactViewSet, basename='contact')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('blog.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('blog.urls')),
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
